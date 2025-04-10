6fdd136d01a386940bcda3facbbddc4428407a4f
Levhomep <165847223+Levhomep@users.noreply.github.com>
2025-04-10 17:38:03 +0200

    Update MpSc1.0.py

--- MpSc1.0.py
+++ MpSc1.0.py
@@ -2,50 +2,45 @@ import customtkinter as ctk
 import time
 import pyautogui
 
-# Startzeitverzögerung
+#  Start
 time.sleep(2)
 
-# Funktion zur Aktualisierung der Position und Farbe
+# Function to get Position and Pixel-colour of mouse
 def piXpoS(_):
-    p = pyautogui.position()  # Mausposition
+    p = pyautogui.position()  # Mouseposition
     try:
-        c = pyautogui.pixel(p[0], p[1])  # Farbe des Pixels
-        text = f"Position: {p}, Farbe: {c}"  # Text zusammenstellen
-        label_info.configure(text=text)  # Text des Labels aktualisieren
+        c = pyautogui.pixel(p[0], p[1])  # Colour of Pixel
+        text = f"Position: {p}, Farbe: {c}"  # Text Collecting
+        label_info.configure(text=text)  # refresh of Text Label
     except OSError:
-        label_info.configure(text="Position außerhalb des Bildschirms!")
-# left = X-Position des linken oberen Punkts   
-# top = Y-Position des oberen Punkts
-# width = Breite des Screenshots
-# height = Höhe des Screenshots
-# region=(left, top, width, height)
-# Funktion für Screenshot
+        label_info.configure(text="Position out of Range!")
+# Funktion for Screenshot
 def screen_shot():
-    screenshot = pyautogui.screenshot()  # Screenshot erstellen
-    screenshot.save("1.png")  # Screenshot speichern
-    label_info.configure(text="Screenshot gespeichert als '1.png'")
+    screenshot = pyautogui.screenshot()  # create Screenshot (x,y,widght,high) add parameter for section Screenshot, empty "()" takes fullscreen
+    screenshot.save("1.png")  # saving Screenshot in same folder as the script
+    label_info.configure(text="Screenshot saved as '1.png'")
 
-# Hauptfenster erstellen
-ctk.set_appearance_mode("Dark")  # Erscheinungsmodus (System, Dark, Light)
-hauptfenster = ctk.CTk()  # Erstellen des Hauptfensters
+# Create Mainwindow
+ctk.set_appearance_mode("Dark")  #  (System, Dark, Light)
+hauptfenster = ctk.CTk()  # Creation Mainwindow
 hauptfenster.geometry("500x200")
-hauptfenster.title("MpSc 1.0")  # Titel des Fensters
-hauptfenster.eval('tk::PlaceWindow . center')  # Fenster zentrieren
-# Hinweistext-Label für Position und Farbe
-label_info = ctk.CTkLabel(hauptfenster, text="Drücke 'h', um die Position und Farbe der Maus zu sehen!", font=("Arial", 14 ,"bold"))
+hauptfenster.title("MpSc 1.0")  # Titel of the Window
+hauptfenster.eval('tk::PlaceWindow . center')  # Mainwindow starts in Center Position
+# Infotext-Label at Start
+label_info = ctk.CTkLabel(hauptfenster, text="Press 'h', to see Mousepostion and Pixelcolour!", font=("Arial", 14 ,"bold"))
 label_info.pack(pady=20)
 
-# Button für Screenshot
+# Button for Screenshot
 button_screenshot = ctk.CTkButton(hauptfenster, text="Take Screenshot", width=200, height=40, command=screen_shot)
 button_screenshot.pack(pady=10)
 
 # Beenden-Button
-button_exit = ctk.CTkButton(hauptfenster, text="Beenden", width=200, height=40, command=hauptfenster.destroy)
+button_exit = ctk.CTkButton(hauptfenster, text="Leave", width=200, height=40, command=hauptfenster.destroy)
 button_exit.pack(pady=10)
 
-# Key-Binding für 'h'
+# Key-Binding for 'h'
 hauptfenster.bind('<h>', piXpoS)
 
-# Hauptloop starten
+# Mainloop start
 hauptfenster.mainloop()
 
